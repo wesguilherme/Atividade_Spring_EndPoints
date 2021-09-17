51dccfe045d0a3c411dd539a06bdd880a8adfb6a
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-09-17 15:51:26 -0300

    update

--- src/main/java/com/example/demo/controllers/Retangulo.java
+++ src/main/java/com/example/demo/controllers/Retangulo.java
@@ -1,4 +1,37 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class Retangulo {
+
+    @GetMapping("/area")
+
+    public String calculaArea(double ladoA, double ladoB) {
+        Double areaTotal = ladoA * ladoB;
+        return "A área do retangulo é: " + areaTotal + " metros quadrados";
+    }
+
+    @GetMapping("/perimetro")
+
+    public String calculaPerimetro(double ladoA, double ladoB) {
+        Double perimetro = ladoA + ladoA + ladoB + ladoB;
+        return "O perímetro do retangulo é: " + perimetro + " metros";
+    }
+
+    @GetMapping("/piso")
+
+    public String quantidadePiso(double areaLajota, double areaTotal) {
+        Double piso = areaTotal / areaLajota;
+        return "A quantidade de piso necessária é de " + piso + " unidades";
+    }
+
+    @GetMapping("/rodape")
+
+    public String quantidadeRodape(double comprimentoLajota, double perimetro) {
+        Double rodape = perimetro / comprimentoLajota;
+        return "A quantidade de rodapé necessária é de " + rodape + " unidades";
+    }
 }
