6c1d805941af3cf831a2b4ff963557d203644220
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-09-17 15:37:16 -0300

    update

--- src/main/java/com/example/demo/controllers/ManipulaStringEndPoints.java
+++ src/main/java/com/example/demo/controllers/ManipulaStringEndPoints.java
@@ -1,15 +1,23 @@
 package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class PalindromoEndPoints {
+public class ManipulaStringEndPoints {
+
+    @GetMapping("/inverte")
+
+    public String inverteString(@RequestParam String nome) {
+        String t = new StringBuilder(nome).reverse().toString();
+        return t;
+    }
 
     @GetMapping("/palindromo")
 
-    public String palindromo(String nome) {
+    public String palindromo(@RequestParam String nome) {
         String nomet = nome.replaceAll("\\s+", "");
         String t = new StringBuilder(nomet).reverse().toString();
         if (nomet.equalsIgnoreCase(t)) {
